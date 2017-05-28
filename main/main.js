141339daa20b34761504d5faaf991b9052359300
unknown <>
2017-05-28 17:54:30 +0800

    homework4

--- main/main.js
+++ main/main.js
@@ -1,5 +1,7 @@
 module.exports = function main(name,age,klass) {
     console.log("Debug Info");
+	function Person(){}
+	function Student(){}
 	Person a=new Person(name,age);
 	Student b=new Student(name,age,klass);
 	a.introduce();
