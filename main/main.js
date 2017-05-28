9b3c8402c2408d892f8dc8f7a97f99139edf4fb7
unknown <>
2017-05-28 18:23:21 +0800

    homework4

--- main/main.js
+++ main/main.js
@@ -1,3 +1,5 @@
+<script type="text/javascript src="/main/person.js"/>
+<script type="text/javascript src="/main/student.js"/>
 module.exports = function main(name,age,klass) {
     console.log("Debug Info");
 	/*var a=new Person(name,age);
