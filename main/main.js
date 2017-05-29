e082bf7b1a50408e00a0ebf14d3f49f0d54c650f
unknown <>
2017-05-29 16:12:38 +0800

    homework4

--- main/main.js
+++ main/main.js
@@ -1,5 +1,21 @@
-require("../main/person.js");
-require("../main/student.js");
-module.exports = function main(name,age,klass) {
+module.exports = function main(numArray) {
     console.log("Debug Info");
+	numArray.forEach(function (element,index,array){
+		if(Count == 0 ){
+			MAX = Number(element);
+			MIN = Number(element);
+			alert(MAX);
+		}
+		if(Number(element)>Number(MAX)){
+			MAX=element;
+		}
+		if(Number(element)<Number(MIN)){
+			MIN=element;
+		}
+		Count++;
+		Sum+=element;
+	});
+	var Average=Number(Sum)/Number(Count);
+	alert("o) 最小值 = "+MIN+"\n"+"o) 最大值 = "+MAX+"\n"+"o) 平均数量 = "Count+"\n"+"o) 平均值 = "+Average.toFixed(2));
+	}
 };
