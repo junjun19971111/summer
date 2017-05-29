8309bcaf9860971744aab54c9a1a52fa7357172f
unknown <>
2017-05-29 13:19:03 +0800

    homework4

--- main/person.js
+++ main/person.js
@@ -1,7 +1,7 @@
 function Person(name,age){
 	this.name=name;
 	this.age=age;
-	this.introduce=function(){
-    return "my name is " + this.name+".I am"+this.age+"years old.";	
-	};
 }
+Person.prototype.introduce=function(){
+    return "my name is " + this.name+".I am "+this.age+" years old.";	
+	};
--- main/student.js
+++ main/student.js
@@ -2,10 +2,10 @@ require("../main/person.js");
 function Student(name,age,klass){
 	Person.call(this,name,age);
 	this.klass=klass;
-	this.introduce=function(){
-	return "my name is " + this.name+".I am"+this.age+"years old.I'm at class"+this.klass;
-	};
-	
 }
+
+Student.prototype.introduce = function(){
+	return "my name is " + this.name+".I am "+this.age+" years old.I'm at class"+this.klass;
+	};
 
 
