c2c27f6d79a47e5c46f18ddd01a421052abee2e6
unknown <>
2017-05-29 16:26:28 +0800

    homework5

--- main/main.js
+++ main/main.js
@@ -15,6 +15,5 @@ module.exports = function main(numArray) {
 		Sum+=element;
 	});
 	var Average=Number(Sum)/Number(Count);
-	alert("o) 最小值 = "+MIN+"\n"+"o) 最大值 = "+MAX+"\n"+"o) 平均数量 = "Count+"\n"+"o) 平均值 = "+Average.toFixed(2));
-	
+	console.log("o) 最小值 = "+MIN+"\n"+"o) 最大值 = "+MAX+"\n"+"o) 平均数量 = "Count+"\n"+"o) 平均值 = "+Average.toFixed(2));
 };
