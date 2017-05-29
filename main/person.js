869c1711ebc0aa74e83b631d01d91274dd820e32
unknown <>
2017-05-29 15:14:02 +0800

    homework4

--- main/person.js
+++ main/person.js
@@ -1,7 +1,7 @@
 module.exports = function Person(name,age){
 	this.name=name;
 	this.age=age;
+	this.introduce=function(){
+		return "my name is " + this.name+".I am "+this.age+" years old.";	
+	};
 }
-Person.prototype.introduce=function(){
-    return "my name is " + this.name+".I am "+this.age+" years old.";	
-};
