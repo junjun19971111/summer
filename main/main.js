8fa4e21384a9dce579197cd43b7f8a9dc5a23e3f
unknown <>
2017-05-29 16:33:33 +0800

    homework5

--- main/main.js
+++ main/main.js
@@ -1,6 +1,9 @@
 module.exports = function main(numArray) {
-	numArray.forEach(function (element,index,array){
-		if(Count == 0 ){
+   var Count=0;
+   var MAX,MIN;
+   var Sum=0;
+    for(var element of numArray)	{
+	if(Count == 0 ){
 			MAX = Number(element);
 			MIN = Number(element);
 			alert(MAX);
@@ -13,7 +16,7 @@ module.exports = function main(numArray) {
 		}
 		Count++;
 		Sum+=element;
-	});
+	}
 	var Average=Number(Sum)/Number(Count);
 	console.log("o) 最小值 = "+MIN+"\n"+"o) 最大值 = "+MAX+"\n"+"o) 平均数量 = "+Count+"\n"+"o) 平均值 = "+Average.toFixed(2));
 };
