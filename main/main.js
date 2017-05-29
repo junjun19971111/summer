e6867619bcab279ac343a03be4f617013dcc93ab
unknown <>
2017-05-29 13:02:06 +0800

    homework4

--- main/main.js
+++ main/main.js
@@ -1,5 +1,5 @@
-<script language="text/javascript"src="./person.js"/>
-<script language="javascript" src="./student.js"/>
+document.write(<script type="text/javascript" src="person.js"></script>);
+document.write(<script type="text/javascript" src="student.js"></script>);
 module.exports = function main(name,age,klass) {
     console.log("Debug Info");
 	var a=new Person(name,age);
--- main/student.js
+++ main/student.js
@@ -1,11 +1,4 @@
-function Person(name,age){
-	this.name=name;
-	this.age=age;
-	this.introduce=function(){
-    return "my name is " + this.name+".I am"+this.age+"years old.";	
-	};
-}
-
+document.write(<script type="text/javascript" src="person.js"></script>);
 function Student(name,age,klass){
 	Person.call(this,name,age);
 	this.klass=klass;
