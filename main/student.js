cceee9fb79838dc9645057d944813b5f0e94126b
unknown <>
2017-05-29 15:25:04 +0800

    homework4

--- main/student.js
+++ main/student.js
@@ -3,7 +3,7 @@ module.exports = function Student(name,age,klass){
 	Person.call(this,name,age);
 	this.klass=klass;
 	this.introduce = function(){
-		return "My name is " + this.name+". I am "+this.age+" years old. I'm at class"+this.klass;
+		return "My name is " + this.name+". I am "+this.age+" years old. I am a Student. I am at Class "+this.klass+".";
 	};
 }
 
