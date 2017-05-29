ad3e9bd884b6c3b96780b8554a407dc7829a61ae
unknown <>
2017-05-29 13:05:59 +0800

    homework4

--- main/student.js
+++ main/student.js
@@ -1,4 +1,4 @@
-document.write(<script type="text/javascript" src="person.js"></script>);
+document.write("<script type='text/javascript' src='person.js'></script>");
 function Student(name,age,klass){
 	Person.call(this,name,age);
 	this.klass=klass;
