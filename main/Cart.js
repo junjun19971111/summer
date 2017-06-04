b4df09ce907046cb2c2989d0f8093d7c43a1a2e3
unknown <>
2017-06-04 15:38:41 +0800

    homework6

--- main/Cart.js
+++ main/Cart.js
@@ -3,15 +3,15 @@
 	this.Count=Count;//HashMap<GoodsID,Count>
 	this.printBuyList = function(){
 		var totals = Number(0);
-		Console.log("***<没钱赚商店>购物清单***");
+		console.log("***<没钱赚商店>购物清单***");
 		for(var item of Goods){
 			var goodsNO = item.getNO();	
 			var num = Count.get(goodsNO);
 			var total = Number(num)*Number(item.getPrice());
 			totals += Number(total);
-			Console.log("名称："+item.getName()+"，数量："+num+"瓶，单价："+item.getPrice().toFixed(2)+"(元)，小计："+total.toFixed(2)+"(元)");
+			console.log("名称："+item.getName()+"，数量："+num+"瓶，单价："+item.getPrice().toFixed(2)+"(元)，小计："+total.toFixed(2)+"(元)");
 		}
-		Console.log("----------------------\n总计："+totals+"(元)\n**********************");
+		console.log("----------------------\n总计："+totals+"(元)\n**********************");
 	}
 
 
