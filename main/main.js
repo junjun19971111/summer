2e75b96e4a1d46902a17459a2ac90cc95a16521c
unknown <>
2017-06-04 18:48:44 +0800

    homework6

--- main/main.js
+++ main/main.js
@@ -1,15 +1,6 @@
 const Pos = require("../main/pos.js");
-const Item = require("../main/Item.js");
 
 module.exports = function main(goodsArray) {
-		var Goods = new Map();
-		var cola = new Item('ITEM000000','可口可乐','3.00'); 
-		var spirit = new Item('ITEM000001','雪碧','3.00');
-		var battery = new Item('ITEM000004','电池','2.00');
-		Goods.set('ITEM000000',cola);
-		Goods.set('ITEM000001',spirit);
-		Goods.set('ITEM000004',battery);
-
 		var goodsMap = new Map();
 		for(var element of goodsArray){
 			if(goodsMap.has(element)){
@@ -20,7 +11,7 @@ module.exports = function main(goodsArray) {
 				goodsMap.set(element,1);	
 			}
 		}
-		var pos = new Pos(Goods,goodsMap);
+		var pos = new Pos(goodsMap);
 		pos.printInventory();
 	
 	}
--- main/pos.js
+++ main/pos.js
@@ -1,7 +1,14 @@
-module.exports = function Pos(Goods,Count){
-	this.Goods = Goods;//HashMap<GoodsID,Item>
+const Item = require("../main/Item.js");
+module.exports = function Pos(Count){
 	this.Count = Count;//HashMap<GoodsID,Count>
 	this.printInventory = function(){
+		var Goods = new Map();
+		var cola = new Item('ITEM000000','可口可乐','3.00'); 
+		var spirit = new Item('ITEM000001','雪碧','3.00');
+		var battery = new Item('ITEM000004','电池','2.00');
+		Goods.set('ITEM000000',cola);
+		Goods.set('ITEM000001',spirit);
+		Goods.set('ITEM000004',battery);
 		var totals = Number(0);
 		var str = "***<没钱赚商店>购物清单***\n";
 		Count.forEach(function(value,key,map){
