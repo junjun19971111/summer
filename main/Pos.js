6bd124d970273da991e2ff8c923c80193efa82bf
unknown <>
2017-06-04 17:46:41 +0800

    homework6

--- main/Pos.js
+++ main/Pos.js
@@ -13,9 +13,9 @@ module.exports = function Pos(Cart){
 			var num = Count.get(goodsNO);
 			var total = Number(num)*Number(item.getPrice());
 			totals += Number(total);
-			str += "名称："+item.getName()+"，数量："+num+"瓶，单价："+item.getPrice()+"(元)，小计："+total.toFixed(2)+"(元)\n";
+	//		str += "名称："+item.getName()+"，数量："+num+"瓶，单价："+item.getPrice()+"(元)，小计："+total.toFixed(2)+"(元)\n";
 		});
-		str += "----------------------\n总计："+totals+"(元)\n**********************";
+	//	str += "----------------------\n总计："+totals+"(元)\n**********************";
 		return str;
 	};	
 
