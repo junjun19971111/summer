0ca9060f7c5726578bf709bf02f9638c8092c0e3
unknown <>
2017-06-04 17:34:53 +0800

    homework6

--- main/main.js
+++ main/main.js
@@ -1,26 +1,28 @@
 const Cart = require("../main/Cart.js");
 const Item = require("../main/Item.js");
 
-module.exports = function pos(goodsArray) {
-    var Goods = new Map();
-	var cola = new Item('ITEM000000','可口可乐','3.00'); 
-	var spirit = new Item('ITEM000001','雪碧','3.00');
-	var battery = new Item('ITEM000004','电池','2.00');
-	Goods.set('ITEM000000',cola);
-	Goods.set('ITEM000001',spirit);
-	Goods.set('ITEM000004',battery);
+module.exports = function main(goodsArray) {
+	describe(pos, (goodsArray) => {
+		var Goods = new Map();
+		var cola = new Item('ITEM000000','可口可乐','3.00'); 
+		var spirit = new Item('ITEM000001','雪碧','3.00');
+		var battery = new Item('ITEM000004','电池','2.00');
+		Goods.set('ITEM000000',cola);
+		Goods.set('ITEM000001',spirit);
+		Goods.set('ITEM000004',battery);
 
-	var goodsMap = new Map();
-	for(var element of goodsArray){
-		if(goodsMap.has(element)){
-			var num = Number(goodsMap.get(element));
-			num++;
-			goodsMap.set(element,num);
-		}else{
-			goodsMap.set(element,1);	
+		var goodsMap = new Map();
+		for(var element of goodsArray){
+			if(goodsMap.has(element)){
+				var num = Number(goodsMap.get(element));
+				num++;
+				goodsMap.set(element,num);
+			}else{
+				goodsMap.set(element,1);	
+			}
 		}
+		var cart = new Cart(Goods,goodsMap);
+		var result = cart.printBuyList();
+		console.log(result);
 	}
-	var cart = new Cart(Goods,goodsMap);
-	var result = cart.printBuyList();
-	console.log(result);
 }
