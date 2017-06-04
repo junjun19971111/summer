a9e1a0a7fd8583b01ba568411a7827bec89a2798
unknown <>
2017-06-04 16:18:46 +0800

    homework6

--- main/Cart.js
+++ main/Cart.js
@@ -1,19 +1,13 @@
 ﻿const Item = require("../main/Item.js");
 
-module.exports = function Cart(Count){
-	var goods = new Set<Item>();
-	var cola = new Item('ITEM000000','可口可乐','3.00'); 
-	var spirit = new Item('ITEM000001','雪碧','3.00');
-	var battery = new Item('ITEM000004','电池','2.00');
-	goods.add(cola);
-	goods.add(spirit);
-	goods.add(battery);
-
-	this.Count=Count;//HashMap<GoodsID,Count>
+module.exports = function Cart(Goods,Count){
+	this.Goods = Goods;//HashMap<GoodsID,Item>
+	this.Count = Count;//HashMap<GoodsID,Count>
 	this.printBuyList = function(){
 		var totals = Number(0);
 		console.log("***<没钱赚商店>购物清单***");
-		for(var item of goods){
+		for(var key in Count){	
+			Item item = Goods.get(Count[key]);
 			var goodsNO = item.getNO();	
 			var num = Count.get(goodsNO);
 			var total = Number(num)*Number(item.getPrice());
--- main/main.js
+++ main/main.js
@@ -1,7 +1,15 @@
 const Cart = require("../main/Cart.js");
 
 module.exports = function main(goodsArray) {
-    var goodsMap = new Map();
+    var Goods = new Map();
+	var cola = new Item('ITEM000000','可口可乐','3.00'); 
+	var spirit = new Item('ITEM000001','雪碧','3.00');
+	var battery = new Item('ITEM000004','电池','2.00');
+	Goods.set('ITEM000000',cola);
+	Goods.add('ITEM000001',spirit);
+	Goods.add('ITEM000004',battery);
+
+	var goodsMap = new Map();
 	for(var element of goodsArray){
 		if(goodsMap.has(element)){
 			var num = Number(goodsMap.get(element));
@@ -11,6 +19,6 @@ module.exports = function main(goodsArray) {
 			goodsMap.set(element,1);	
 		}
 	}
-	var cart = new Cart(goodsMap);
+	var cart = new Cart(Goods,goodsMap);
 	cart.printBuyList();
 }
