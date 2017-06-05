fb74b3b14d1527406040b32db181d3d64ad5f6a5
MforMargarite <shoreline0209@163.com>
2017-06-05 15:46:32 +0800

    homework7

--- main/main.js
+++ main/main.js
@@ -1,7 +1,7 @@
 module.exports = function main(emailAddr,suffixArr) {
 	var splits = emailAddr.split('@');
 	var suffix = splits[splits.length-1];
-	suffixArr.forEach(function(elem,index){
+	suffixArr.forEach(function(elem){
 		if(elem == suffix)
 			return true;
 	});
