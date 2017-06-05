68a477c2cb5fbd2ccfd98a20850e0ca23865c64d
MforMargarite <shoreline0209@163.com>
2017-06-05 11:24:49 +0800

    homework6

--- main/main.js
+++ main/main.js
@@ -1,13 +1,13 @@
 const Cart = require("../main/Cart.js");
 
 module.exports = function main(goodsArray) {
-		var Goods = new Map();
+		var goods = new Map();
 		var cola = new Item('ITEM000000','可口可乐','3.00'); 
 		var spirit = new Item('ITEM000001','雪碧','3.00');
 		var battery = new Item('ITEM000004','电池','2.00');
-		Goods.set('ITEM000000',cola);
-		Goods.set('ITEM000001',spirit);
-		Goods.set('ITEM000004',battery);
+		goods.set('ITEM000000',cola);
+		goods.set('ITEM000001',spirit);
+		goods.set('ITEM000004',battery);
 	
 		var goodsMap = new Map();
 		for(var element of goodsArray){
@@ -19,7 +19,7 @@ module.exports = function main(goodsArray) {
 				goodsMap.set(element,1);	
 			}
 		}
-		var cart = new Cart(Goods,goodsMap);
+		var cart = new Cart(goods,goodsMap);
 		return cart.printInventory();
 	
 	}
