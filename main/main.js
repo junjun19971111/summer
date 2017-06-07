90c89f30b2a160f0c3e27a18270744eff0325f25
MforMargarite <shoreline0209@163.com>
2017-06-07 16:39:11 +0800

    homework8

--- main/main.js
+++ main/main.js
@@ -1,70 +1,71 @@
 module.exports = function main(num) {
-	var str = new Array(num.length);//array [row]=content
-	var numMap = new Map();
-	for(var i=0;i<num.length;i++){
+
+	var str = new Array(3);
+	for(var i=0;i<3;i++){
 		str[i] = "";
-	}
+	}//array [row]=content
+	var numMap = new Map();
 	var numArr = num.split("");
 	for(var i=0;i<=9;i++){
-		var each = new Map();//Map<"rowcol",val>
+		var map = new Map();//Map<"rowcol",val>
 		switch(i){
 			case 0:
-				each.set("00",0);each.set("01",1);each.set("02",0);
-				each.set("10",2);each.set("11",0);each.set("12",2);
-				each.set("20",2);each.set("21",1);each.set("22",2);
+				map.set("00",0);map.set("01",1);map.set("02",0);
+				map.set("10",2);map.set("11",0);map.set("12",2);
+				map.set("20",2);map.set("21",1);map.set("22",2);
 				break;
 			case 1:
-				each.set("00",0);each.set("01",0);each.set("02",0);
-				each.set("10",0);each.set("11",0);each.set("12",2);
-				each.set("20",0);each.set("21",0);each.set("22",2);
+				map.set("00",0);map.set("01",0);map.set("02",0);
+				map.set("10",0);map.set("11",0);map.set("12",2);
+				map.set("20",0);map.set("21",0);map.set("22",2);
 				break;
 			case 2:
-				each.set("00",0);each.set("01",1);each.set("02",0);
-				each.set("10",0);each.set("11",1);each.set("12",2);
-				each.set("20",2);each.set("21",1);each.set("22",0);
+				map.set("00",0);map.set("01",1);map.set("02",0);
+				map.set("10",0);map.set("11",1);map.set("12",2);
+				map.set("20",2);map.set("21",1);map.set("22",0);
 				break;	
 			case 3:
-				each.set("00",0);each.set("01",1);each.set("02",0);
-				each.set("10",0);each.set("11",1);each.set("12",2);
-				each.set("20",0);each.set("21",1);each.set("22",2);
+				map.set("00",0);map.set("01",1);map.set("02",0);
+				map.set("10",0);map.set("11",1);map.set("12",2);
+				map.set("20",0);map.set("21",1);map.set("22",2);
 				break;
 			case 4:
-				each.set("00",0);each.set("01",0);each.set("02",0);
-				each.set("10",2);each.set("11",1);each.set("12",2);
-				each.set("20",0);each.set("21",0);each.set("22",2);
+				map.set("00",0);map.set("01",0);map.set("02",0);
+				map.set("10",2);map.set("11",1);map.set("12",2);
+				map.set("20",0);map.set("21",0);map.set("22",2);
 				break;
 			case 5:
-				each.set("00",0);each.set("01",1);each.set("02",0);
-				each.set("10",2);each.set("11",1);each.set("12",0);
-				each.set("20",0);each.set("21",1);each.set("22",2);
+				map.set("00",0);map.set("01",1);map.set("02",0);
+				map.set("10",2);map.set("11",1);map.set("12",0);
+				map.set("20",0);map.set("21",1);map.set("22",2);
 				break;
 			case 6:
-				each.set("00",0);each.set("01",1);each.set("02",0);
-				each.set("10",2);each.set("11",1);each.set("12",0);
-				each.set("20",2);each.set("21",1);each.set("22",2);
+				map.set("00",0);map.set("01",1);map.set("02",0);
+				map.set("10",2);map.set("11",1);map.set("12",0);
+				map.set("20",2);map.set("21",1);map.set("22",2);
 				break;
 			case 7:
-				each.set("00",0);each.set("01",1);each.set("02",0);
-				each.set("10",0);each.set("11",0);each.set("12",0);
-				each.set("20",0);each.set("21",0);each.set("22",2);
+				map.set("00",0);map.set("01",1);map.set("02",0);
+				map.set("10",0);map.set("11",0);map.set("12",2);
+				map.set("20",0);map.set("21",0);map.set("22",2);
 				break;
 			case 8:
-				each.set("00",0);each.set("01",1);each.set("02",0);
-				each.set("10",2);each.set("11",1);each.set("12",2);
-				each.set("20",2);each.set("21",1);each.set("22",2);
+				map.set("00",0);map.set("01",1);map.set("02",0);
+				map.set("10",2);map.set("11",1);map.set("12",2);
+				map.set("20",2);map.set("21",1);map.set("22",2);
 				break;								
 			case 9:
-				each.set("00",0);each.set("01",1);each.set("02",0);
-				each.set("10",2);each.set("11",1);each.set("12",2);
-				each.set("20",0);each.set("21",0);each.set("22",2);
+				map.set("00",0);map.set("01",1);map.set("02",0);
+				map.set("10",2);map.set("11",1);map.set("12",2);
+				map.set("20",0);map.set("21",0);map.set("22",2);
 				break;		
 		}
-		numMap.set(i,each);
+		numMap.set(i,map);
 	}
 	for(var i of numArr){
 		var myeach = numMap.get(Number(i));
 		for(var row = 0;row<3;row++){
-			for(var col=0;col<num.length;col++){//数字的位数
+			for(var col=0;col<3;col++){//数字的位数
 				switch(myeach.get(row+""+col)){
 					case 0:str[row]+='.';break;
 					case 1:str[row]+='_';break;
